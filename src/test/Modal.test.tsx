846178fb7878627746308c6a971b981f52ecb4d9
h-genie <ses2000hj@hanmail.net>
2024-12-16 00:45:55 +0900

    test : Modal 테스트

--- src/test/Modal.test.tsx
+++ src/test/Modal.test.tsx
@@ -1,9 +1,10 @@
 import { render, screen } from "@testing-library/react"
+import userEvent from "@testing-library/user-event"
+import { describe, it, expect, vi } from "vitest"
 import Modal from "../components/Modal"
-import { describe, it, expect } from "vitest"
 
 describe("Modal Component", () => {
-  it("renders children when open", () => {
+  it("모달이 열려 있을때 자식요소 렌더링", () => {
     render(
       <Modal open={true} onClose={() => {}}>
         <div>Modal Content</div>
@@ -13,7 +14,7 @@ describe("Modal Component", () => {
     expect(screen.getByText("Modal Content")).toBeInTheDocument()
   })
 
-  it("does not render children when closed", () => {
+  it("모달이 닫혀잇을때 자식요소 렌더링하지 않음", () => {
     render(
       <Modal open={false} onClose={() => {}}>
         <div>Modal Content</div>
@@ -21,5 +22,47 @@ describe("Modal Component", () => {
     )
 
     expect(screen.queryByText("Modal Content")).not.toBeInTheDocument()
+  })
+
+  it("닫기 버튼 눌렀을 때 onClose 함수 호출", async () => {
+    const handleClose = vi.fn()
+    render(
+      <Modal open={true} onClose={handleClose}>
+        <div>Modal Content</div>
+      </Modal>
+    )
+
+    const closeButton = screen.getByRole("button")
+    await userEvent.click(closeButton)
+
+    expect(handleClose).toHaveBeenCalledTimes(1)
+  })
+
+  it("모달 바깥쪽 클릭했을 때 onClose 함수 호출", async () => {
+    const handleClose = vi.fn()
+    render(
+      <Modal open={true} onClose={handleClose}>
+        <div>Modal Content</div>
+      </Modal>
+    )
+
+    const dialogElement = screen.getByRole("dialog")
+    await userEvent.click(dialogElement)
+
+    expect(handleClose).toHaveBeenCalledTimes(1)
+  })
+
+  it("사용자정의 스타일과 불투명도 적용", () => {
+    const customStyle = { color: "red" }
+    render(
+      <Modal open={true} onClose={() => {}} style={customStyle} backdrop={0.5}>
+        <div>Styled Modal</div>
+      </Modal>
+    )
+
+    const dialogElement = screen.getByRole("dialog")
+
+    expect(dialogElement).toHaveStyle({ color: "rgb(255, 0, 0)" })
+    expect(dialogElement).toHaveStyle({ "--backdrop-opacity": "0.5" })
   })
 })
