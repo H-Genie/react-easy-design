4cac4a05aa81b4fe6f072cc66a47fe1cc17fce9a
h-genie <ses2000hj@hanmail.net>
2024-12-16 22:23:42 +0900

    test : 커버리지 측정

--- vite.config.ts
+++ vite.config.ts
@@ -7,6 +7,10 @@ export default defineConfig({
   test: {
     globals: true,
     environment: "jsdom",
-    setupFiles: "./src/test/setup.ts"
+    setupFiles: "./src/test/setup.ts",
+    coverage: {
+      provider: "v8",
+      reporter: ["text", "json", "html"]
+    }
   }
 })
