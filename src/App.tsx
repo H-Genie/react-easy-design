b7ae18ddaa489e29f668bb5eb59485ae8c5eef8e
h-genie <ses2000hj@hanmail.net>
2024-12-21 15:11:19 +0900

    release : 1.0.2

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,3 @@
-import { ScrollNavigation } from "react-easy-design"
-
 export default function App() {
-  return (
-    <>
-      <ScrollNavigation
-        items={["reservation", "products", "location", "promotion"]}
-        top={100}
-        backgroundColor="yellow"
-        textColor="black"
-      />
-
-      {/* contents to example display */}
-      <div id="reservation" style={{ height: "100vh" }}>
-        reservation
-      </div>
-      <div id="products" style={{ height: "100vh" }}>
-        items
-      </div>
-      <div id="location" style={{ height: "100vh" }}>
-        location
-      </div>
-      <div id="promotion" style={{ height: "100vh" }}>
-        promotion
-      </div>
-    </>
-  )
+  return <div>App</div>
 }
